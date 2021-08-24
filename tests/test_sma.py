aa79609f7b792a6e4a75fb2eba0c62082b196e70
LeeDongGeon <secmatth1996@gmail.com>
2021-08-24 21:05:38 +0900

    Remove sma_extended test from test_sma.py

--- tests/test_sma.py
+++ tests/test_sma.py
@@ -19,22 +19,6 @@ class TestSMA(TestBase):
         self.assertEqual(255.5500, round(float(results[249].Sma), 4))
         self.assertEqual(251.8600, round(float(results[501].Sma), 4))
 
-    # TODO: Move to test for sma_extended.
-    # def test_extended(self):
-    #     results = indicators.get_sma_extended(self.quotes, 20)
-
-    #     # proper quantities
-    #     # should always be the same number of results as there is quotes
-    #     self.assertEqual(502, len(results))
-    #     self.assertEqual(483, len(list(filter(lambda x: x.Sma is not None, results))))
-
-    #     # sample values
-    #     r = results[501]
-    #     self.assertEqual(251.86, float(r.Sma));
-    #     self.assertEqual(9.45, float(r.Mad));
-    #     self.assertEqual(119.2510, round(float(r.Mse), 4))
-    #     self.assertEqual(0.037637, round(float(r.Mape), 6))
-
     def test_bad_data(self):
         results = indicators.get_sma_extended(self.bad_quotes, 15)
 
