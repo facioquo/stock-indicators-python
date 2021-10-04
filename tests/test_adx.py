123de9f5c00ef1231b569bebfe39f92027764441
Dong-Geon Lee <secmatth1996@gmail.com>
2021-10-04 20:59:06 +0900

    Refactor tests to use session scoped fixtures

--- tests/test_adx.py
+++ tests/test_adx.py
@@ -1,54 +1,56 @@
-from tests.testbase import TestBase
+import pytest
 from SkenderStockIndicators import indicators
 
-class TestAdx(TestBase):
-    def test_standard(self):
-        results = indicators.get_adx(self.quotes, 14)
+class TestAdx:
+    def test_standard(self, quotes):
+        results = indicators.get_adx(quotes, 14)
 
         # proper quantities
         # should always be the same number of results as there is quotes
-        self.assertEqual(502, len(results))
-        self.assertEqual(475, len(list(filter(lambda x: x.adx is not None, results))))
+        assert 502 == len(results)
+        assert 475 == len(list(filter(lambda x: x.adx is not None, results)))
 
         # sample values
         r = results[19]
-        self.assertEqual(21.0361, round(float(r.pdi), 4))
-        self.assertEqual(25.0124, round(float(r.mdi), 4))
-        self.assertIsNone(r.adx)
+        assert 21.0361 == round(float(r.pdi), 4)
+        assert 25.0124 == round(float(r.mdi), 4)
+        assert r.adx is None
 
-        r = results[29]
-        self.assertEqual(37.9719, round(float(r.pdi), 4))
-        self.assertEqual(14.1658, round(float(r.mdi), 4))
-        self.assertEqual(19.7949, round(float(r.adx), 4))
+        r  = results[29]
+        assert 37.9719 == round(float(r.pdi), 4)
+        assert 14.1658 == round(float(r.mdi), 4)
+        assert 19.7949 == round(float(r.adx), 4)
 
         r = results[248]
-        self.assertEqual(32.3167, round(float(r.pdi), 4))
-        self.assertEqual(18.2471, round(float(r.mdi), 4))
-        self.assertEqual(30.5903, round(float(r.adx), 4))
+        assert 32.3167 == round(float(r.pdi), 4)
+        assert 18.2471 == round(float(r.mdi), 4)
+        assert 30.5903 == round(float(r.adx), 4)
 
         r = results[501]
-        self.assertEqual(17.7565, round(float(r.pdi), 4))
-        self.assertEqual(31.1510, round(float(r.mdi), 4))
-        self.assertEqual(34.2987, round(float(r.adx), 4))
+        assert 17.7565 == round(float(r.pdi), 4)
+        assert 31.1510 == round(float(r.mdi), 4)
+        assert 34.2987 == round(float(r.adx), 4)
 
-    def test_bad_data(self):
-        results = indicators.get_adx(self.bad_quotes, 20)
+    def test_bad_data(self, bad_quotes):
+        results = indicators.get_adx(bad_quotes, 20)
 
-        self.assertEqual(502, len(results))
+        assert 502 == len(results)
 
-    def test_removed(self):
-        results = indicators.get_adx(self.quotes).remove_warmup_periods()
+    def test_removed(self, quotes):
+        results = indicators.get_adx(quotes).remove_warmup_periods()
 
-        self.assertEqual(502 - (2 * 14 + 100), len(results))
+        assert 502 - (2 * 14 + 100) == len(results)
 
         r = results[len(results)-1]
-        self.assertEqual(17.7565, round(float(r.pdi), 4))
-        self.assertEqual(31.1510, round(float(r.mdi), 4))
-        self.assertEqual(34.2987, round(float(r.adx), 4))
+        assert 17.7565 == round(float(r.pdi), 4)
+        assert 31.1510 == round(float(r.mdi), 4)
+        assert 34.2987 == round(float(r.adx), 4)
 
-    def test_exceptions(self):
+    def test_exceptions(self, quotes):
         from System import ArgumentOutOfRangeException
-        self.assertRaises(ArgumentOutOfRangeException, indicators.get_adx, self.quotes, 1)
+        with pytest.raises(ArgumentOutOfRangeException):
+            indicators.get_adx(quotes, 1)
 
         from Skender.Stock.Indicators import BadQuotesException
-        self.assertRaises(BadQuotesException, indicators.get_adx, self.data_reader.get(159), 30)
+        with pytest.raises(BadQuotesException):
+            indicators.get_adx(quotes[:159], 30)
--- tests/test_sma.py
+++ tests/test_sma.py
@@ -1,38 +1,39 @@
-from tests.testbase import TestBase
+import pytest
 from SkenderStockIndicators import indicators
 
-class TestSMA(TestBase):
-
-    def test_standard(self):
-        results = indicators.get_sma(self.quotes, 20)
+class TestSMA:
+    def test_standard(self, quotes):
+        results = indicators.get_sma(quotes, 20)
 
         # proper quantities
         # should always be the same number of results as there is quotes
-        self.assertEqual(502, len(results))
-        self.assertEqual(483, len(list(filter(lambda x: x.sma is not None, results))))
+        assert 502 == len(results)
+        assert 483 == len(list(filter(lambda x: x.sma is not None, results)))
 
         # sample values
-        self.assertIsNone(results[18].sma)
-        self.assertEqual(214.5250, round(float(results[19].sma), 4))
-        self.assertEqual(215.0310, round(float(results[24].sma), 4))
-        self.assertEqual(234.9350, round(float(results[149].sma), 4))
-        self.assertEqual(255.5500, round(float(results[249].sma), 4))
-        self.assertEqual(251.8600, round(float(results[501].sma), 4))
+        assert results[18].sma is None
+        assert 214.5250 == round(float(results[19].sma), 4)
+        assert 215.0310 == round(float(results[24].sma), 4)
+        assert 234.9350 == round(float(results[149].sma), 4)
+        assert 255.5500 == round(float(results[249].sma), 4)
+        assert 251.8600 == round(float(results[501].sma), 4)
 
-    def test_bad_data(self):
-        results = indicators.get_sma_extended(self.bad_quotes, 15)
+    def test_bad_data(self, bad_quotes):
+        results = indicators.get_sma_extended(bad_quotes, 15)
 
-        self.assertEqual(502, len(results))
+        assert 502 == len(results)
 
-    def test_removed(self):
-        results = indicators.get_sma(self.quotes, 20).remove_warmup_periods()
+    def test_removed(self, quotes):
+        results = indicators.get_sma(quotes, 20).remove_warmup_periods()
 
-        self.assertEqual(483, len(results))
-        self.assertEqual(251.8600, round(float(results[len(results)-1].sma), 4))
+        assert 483 == len(results)
+        assert 251.8600 == round(float(results[len(results)-1].sma), 4)
 
-    def test_exceptions(self):
+    def test_exceptions(self, quotes):
         from System import ArgumentOutOfRangeException
-        self.assertRaises(ArgumentOutOfRangeException, indicators.get_sma, self.quotes, 0)
+        with pytest.raises(ArgumentOutOfRangeException):
+            indicators.get_sma(quotes, 0)
 
         from Skender.Stock.Indicators import BadQuotesException
-        self.assertRaises(BadQuotesException, indicators.get_sma, self.data_reader.get(9), 10)
+        with pytest.raises(BadQuotesException):
+            indicators.get_sma(quotes[:9], 10)
