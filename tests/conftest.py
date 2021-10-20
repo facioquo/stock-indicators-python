c20169fdefb50aab4fbe4e27e1ca1a14900e0d19
Dong-Geon Lee <secmatth1996@gmail.com>
2021-10-20 19:59:04 +0900

    Add more quotes

--- tests/conftest.py
+++ tests/conftest.py
@@ -50,7 +50,6 @@ def bad_quotes(days: int = 502):
     rows = list(wb['Bad'])[1:]
 
     h = []
-    i=1
     for row in rows:
         h.append(Quote(
             # Quoto.date cannot be null.
@@ -61,6 +60,112 @@ def bad_quotes(days: int = 502):
             '{:f}'.format(PyDecimal(row[6].value)) if row[6].value is not None else None,
             '{:f}'.format(PyDecimal(row[7].value)) if row[7].value is not None else None,
             '{:f}'.format(PyDecimal(row[8].value)) if row[8].value is not None else None,
+        ))
+
+    h.reverse()
+    return h[:days]
+
+@pytest.fixture(scope='session')
+def bitcoin_quotes(days: int = 1246):
+    rows = list(wb['Bitcoin'])[1:]
+
+    h = []
+    for row in rows:
+        h.append(Quote(
+            row[3].value,
+            row[4].value,
+            row[5].value,
+            row[6].value,
+            row[7].value,
+            row[8].value,
+        ))
+
+    h.reverse()
+    return h[:days]
+
+@pytest.fixture(scope='session')
+def intraday_quotes(days: int = 1564):
+    rows = list(wb['Intraday'])[1:]
+
+    h = []
+    for row in rows:
+        h.append(Quote(
+            row[3].value,
+            row[4].value,
+            row[5].value,
+            row[6].value,
+            row[7].value,
+            row[8].value,
+        ))
+
+    h.reverse()
+    return h[:days]
+
+@pytest.fixture(scope='session')
+def longish_quotes(days: int = 5285):
+    rows = list(wb['Longish'])[1:]
+
+    h = []
+    for row in rows:
+        h.append(Quote(
+            row[3].value,
+            row[4].value,
+            row[5].value,
+            row[6].value,
+            row[7].value,
+            row[8].value,
+        ))
+
+    h.reverse()
+    return h[:days]
+
+@pytest.fixture(scope='session')
+def longest_quotes():
+    rows = list(wb['Longest'])[1:]
+
+    h = []
+    for row in rows:
+        h.append(Quote(
+            row[3].value,
+            row[4].value,
+            row[5].value,
+            row[6].value,
+            row[7].value,
+            row[8].value,
+        ))
+
+    return h
+
+@pytest.fixture(scope='session')
+def penny_quotes():
+    rows = list(wb['Penny'])[1:]
+
+    h = []
+    for row in rows:
+        h.append(Quote(
+            row[2].value,
+            row[3].value,
+            row[4].value,
+            row[5].value,
+            row[6].value,
+            row[7].value,
+        ))
+
+    return h
+
+@pytest.fixture(scope='session')
+def zigzag_quotes(days: int = 342):
+    rows = list(wb['ZigZag'])[1:]
+
+    h = []
+    for row in rows:
+        h.append(Quote(
+            row[1].value,
+            row[2].value,
+            row[3].value,
+            row[4].value,
+            row[5].value,
+            row[6].value,
         ))
 
     h.reverse()
