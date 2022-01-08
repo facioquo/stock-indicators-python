022f2cdbf9d4c924cfe84316f3221b3fde60f5f9
Dong-Geon Lee <secmatth1996@gmail.com>
2022-01-08 16:00:27 +0900

    Update docstring for Ichimoku

--- stock_indicators/indicators/ichimoku.py
+++ stock_indicators/indicators/ichimoku.py
@@ -7,10 +7,35 @@ from stock_indicators._cstypes import to_pydecimal
 from stock_indicators.indicators.common.results import IndicatorResults, ResultBase
 from stock_indicators.indicators.common.quote import Quote
 
-def get_ichimoku(quotes: Iterable[Quote],
-                 tenkan_periods: int = 9,
-                 kijun_periods: int = 26,
-                 senkou_b_periods: int = 52):
+def get_ichimoku(quotes: Iterable[Quote], tenkan_periods: int = 9,
+                 kijun_periods: int = 26, senkou_b_periods: int = 52):
+    """Get Ichimoku Cloud calculated.
+    
+    Ichimoku Cloud, also known as Ichimoku Kinkō Hyō, is a collection of indicators
+    that depict support and resistance, momentum, and trend direction.
+      
+    Parameters:
+        `quotes` : Iterable[Quotes]
+            Historical price quotes.
+        
+        `tenkan_periods` : int, defaults 9
+            Number of periods in the Tenkan-sen midpoint evaluation.
+        
+        `kijun_periods` : int, defaults 26
+            Number of periods in the shorter Kijun-sen midpoint evaluation.
+            This value is also used to offset Senkou and Chinkou spans.
+        
+        `senkou_b_periods` : int, defaults 52
+            Number of periods in the longer Senkou leading span B midpoint evaluation.
+    
+    Returns:
+        `IchimokuResults[IchimokuResult]`
+            IchimokuResults is list of IchimokuResult with providing useful helper methods.
+    
+    See more:
+         - [Ichimoku Cloud Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/Ichimoku/#content)
+         - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
+    """
     results = CsIndicator.GetIchimoku[Quote](CsList(Quote, quotes),
                                              tenkan_periods,
                                              kijun_periods,
