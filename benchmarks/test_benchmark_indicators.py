7a2ea3ce481b62fd1cb923cc99c90ae4862fd14f
Dong-Geon Lee <secmatth1996@gmail.com>
2022-01-09 15:36:16 +0900

    Update benchmarks

--- benchmarks/test_benchmark_indicators.py
+++ benchmarks/test_benchmark_indicators.py
@@ -26,6 +26,18 @@ def test_benchmark_beta(benchmark, quotes, other_quotes):
     
 def test_benchmark_bollinger_bands(benchmark, quotes):
     benchmark(indicators.get_bollinger_bands, quotes)
+    
+def test_chandelier(benchmark, quotes):
+    benchmark(indicators.get_chandelier, quotes)
+
+def test_donchian(benchmark, quotes):
+    benchmark(indicators.get_donchian, quotes)
+    
+def test_double_ema(benchmark, quotes):
+    benchmark(indicators.get_double_ema, quotes, 20)
+
+def test_elder_ray(benchmark, quotes):
+    benchmark(indicators.get_elder_ray, quotes)
 
 def test_benchmark_ema(benchmark, quotes):
     benchmark(indicators.get_ema, quotes, 20)
@@ -33,11 +45,26 @@ def test_benchmark_ema(benchmark, quotes):
 def test_benchmark_fractal(benchmark, quotes):
     benchmark(indicators.get_fractal, quotes)
 
+def test_heikin_ashi(benchmark, quotes):
+    benchmark(indicators.get_heikin_ashi, quotes)
+
+def test_ichimoku(benchmark, quotes):
+    benchmark(indicators.get_ichimoku, quotes)
+
 def test_benchmark_macd(benchmark, quotes):
     benchmark(indicators.get_macd, quotes)
 
+def test_parabolic_sar(benchmark, quotes):
+    benchmark(indicators.get_parabolic_sar, quotes)
+
+def test_roc(benchmark, quotes):
+    benchmark(indicators.get_roc, quotes, 20)
+
 def test_benchmark_rsi(benchmark, quotes):
     benchmark(indicators.get_rsi, quotes)
+    
+def test_slope(benchmark, quotes):
+    benchmark(indicators.get_slope, quotes, 20)
 
 def test_benchmark_sma_extended(benchmark, quotes):
     benchmark(indicators.get_sma_extended, quotes, 20)
@@ -53,3 +80,9 @@ def test_benchmark_stoch(benchmark, quotes):
 
 def test_benchmark_super_trend(benchmark, quotes):
     benchmark(indicators.get_super_trend, quotes)
+
+def test_triple_ema(benchmark, quotes):
+    benchmark(indicators.get_triple_ema, quotes, 20)
+
+def test_trix(benchmark, quotes):
+    benchmark(indicators.get_trix, quotes, 20, 5)
