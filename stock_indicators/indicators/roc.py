3e22b1fc3a7532a344def177b37ecfa9eee7821d
Dong-Geon Lee <secmatth1996@gmail.com>
2022-01-10 14:44:24 +0900

    Add docstring to ROCWB

--- stock_indicators/indicators/roc.py
+++ stock_indicators/indicators/roc.py
@@ -32,6 +32,32 @@ def get_roc(quotes: Iterable[Quote], lookback_periods: int, sma_periods: int = N
     return ROCResults(results, ROCResult)
 
 def get_roc_with_band(quotes: Iterable[Quote], lookback_periods: int, ema_periods: int, std_dev_periods: int):
+    """Get ROCWB calculated.
+
+    Rate of Change with Bands (ROCWB) is the percent change of Close price
+    over a lookback window with standard deviation bands.
+
+    Parameters:
+        `quotes` : Iterable[Quotes]
+            Historical price quotes.
+
+        `lookback_periods` : int
+            Number of periods in the lookback window.
+
+        `ema_periods` : int
+            Number of periods for the ROC EMA line.
+
+        `std_dev_periods` : int
+            Number of periods the standard deviation for upper/lower band lines.
+
+    Returns:
+        `ROCWBResults[ROCWBResult]`
+            ROCWBResults is list of ROCWBResult with providing useful helper methods.
+
+    See more:
+         - [ROCWB Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/Roc/#content)
+         - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
+    """
     results = CsIndicator.GetRocWb[Quote](CsList(Quote, quotes), lookback_periods, ema_periods, std_dev_periods)
     return ROCWBResults(results, ROCWBResult)
 
