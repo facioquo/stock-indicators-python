37308fffcdb00d1613ebf53aa7378819abe54eaa
DongGeon Lee <secmatth1996@gmail.com>
2022-01-15 15:07:31 +0900

    refactor: Refactor type conversion method to class (#105)

--- stock_indicators/_cstypes/decimal.py
+++ stock_indicators/_cstypes/decimal.py
@@ -1,34 +1,32 @@
 from decimal import Decimal as PyDecimal
 from System import Decimal as CsDecimal
 
-def Decimal(decimal):
+
+class Decimal:
     """
-    Converts a number into C#'s `System.Decimal` class.
+    Class for converting a number into C#'s `System.Decimal` class.
 
-    Parameter
-    ----------
-    decimal : `int`, `float` or any `object` that can be represented as a number.
+    Parameters:
+        decimal : `int`, `float` or any `object` that can be represented as a number.
 
-    Example
-    --------
-    Constructing `System.Decimal` from `float` of Python.
+    Example:
+        Constructing `System.Decimal` from `float` of Python.
 
-    >>> cs_decimal = Decimal(2.5)
-    >>> cs_decimal
-    2.5
+        >>> cs_decimal = Decimal(2.5)
+        >>> cs_decimal
+        2.5
     """
+    
+    def __new__(cls, decimal) -> CsDecimal:
+        return CsDecimal.Parse(str(decimal))
 
-    return CsDecimal.Parse(str(decimal))
 
 def to_pydecimal(cs_decimal):
     """
     Converts an object to a native Python decimal object.
 
-    Parameter
-    ----------
-    cs_decimal : `System.Decimal` of C# or any `object` that can be represented as a number.
-
+    Parameter:
+        cs_decimal : `System.Decimal` of C# or any `object` that can be represented as a number.
     """
-
     if cs_decimal is not None:
         return PyDecimal(str(cs_decimal))
