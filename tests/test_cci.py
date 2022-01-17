4e31c3ea717b72e60966d96c20a677f610d2f5b6
DongGeon Lee <secmatth1996@gmail.com>
2022-01-17 13:06:53 +0900

    feat: Add CCI (#110)

---
+++ tests/test_cci.py
@@ -0,0 +1,33 @@
+import pytest
+from stock_indicators import indicators
+
+class TestCCI:
+    def test_standard(self, quotes):
+        results = indicators.get_cci(quotes, 20)
+        
+        assert 502 == len(results)
+        assert 483 == len(list(filter(lambda x: x.cci is not None, results)))
+        
+        r = results[501]
+        assert -52.9946 == round(float(r.cci), 4)
+        
+    def test_bad_data(self, bad_quotes):
+        r = indicators.get_cci(bad_quotes, 15)
+        assert 502 == len(r)
+    
+    def test_removed(self, quotes):
+        results = indicators.get_cci(quotes, 20).remove_warmup_periods()
+        
+        assert 502 - 19 == len(results)
+        
+        last = results.pop()
+        assert -52.9946 == round(float(last.cci), 4)
+        
+    def test_exceptions(self, quotes):
+        from System import ArgumentOutOfRangeException
+        with pytest.raises(ArgumentOutOfRangeException):
+            indicators.get_cci(quotes, 0)
+        
+        from Skender.Stock.Indicators import BadQuotesException
+        with pytest.raises(BadQuotesException):
+            indicators.get_cci(quotes[:30], 30)
