c2ff45667685963420f63cb15ec560d47fba6a13
Dong-Geon Lee <secmatth1996@gmail.com>
2022-01-18 22:53:48 +0900

    Add big_quotes fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,24 +28,6 @@ def quotes(days: int = 502):
     return h[:days]
 
 @pytest.fixture(scope='session')
-def other_quotes(days: int = 502):
-    rows = list(wb['Compare'])[1:]
-
-    h = []
-    for row in rows:
-        h.append(Quote(
-            row[2].value,
-            row[3].value,
-            row[4].value,
-            row[5].value,
-            row[6].value,
-            row[7].value,
-        ))
-
-    h.reverse()
-    return h[:days]
-
-@pytest.fixture(scope='session')
 def bad_quotes(days: int = 502):
     rows = list(wb['Bad'])[1:]
 
@@ -60,6 +42,42 @@ def bad_quotes(days: int = 502):
             '{:f}'.format(PyDecimal(row[4].value)) if row[4].value is not None else None,
             '{:f}'.format(PyDecimal(row[5].value)) if row[5].value is not None else None,
             '{:f}'.format(PyDecimal(row[6].value)) if row[6].value is not None else None,
+        ))
+
+    h.reverse()
+    return h[:days]
+
+@pytest.fixture(scope='session')
+def big_quotes(days: int = 1246):
+    rows = list(wb['TooBig'])[1:]
+
+    h = []
+    for row in rows:
+        h.append(Quote(
+            row[1].value,
+            row[2].value,
+            row[3].value,
+            row[4].value,
+            row[5].value,
+            row[6].value,
+        ))
+
+    h.reverse()
+    return h[:days]
+
+@pytest.fixture(scope='session')
+def other_quotes(days: int = 502):
+    rows = list(wb['Compare'])[1:]
+
+    h = []
+    for row in rows:
+        h.append(Quote(
+            row[2].value,
+            row[3].value,
+            row[4].value,
+            row[5].value,
+            row[6].value,
+            row[7].value,
         ))
 
     h.reverse()
