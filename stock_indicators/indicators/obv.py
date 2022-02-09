f63a4d3ea32a2829330da02bd56ef3a7ac3425ff
Dong-Geon Lee <secmatth1996@gmail.com>
2022-02-10 00:32:52 +0900

    Add On-balance Volume

---
+++ stock_indicators/indicators/obv.py
@@ -0,0 +1,62 @@
+from typing import Iterable, Optional, TypeVar
+from stock_indicators._cslib import CsIndicator
+from stock_indicators._cstypes import List as CsList
+from stock_indicators.indicators.common.helpers import ToQuotesMixin
+from stock_indicators.indicators.common.results import IndicatorResults, ResultBase
+from stock_indicators.indicators.common.quote import Quote
+
+
+def get_obv(quotes: Iterable[Quote], sma_periods: Optional[int] = None):
+    """Get OBV calculated.
+
+    On-balance Volume (OBV) is a rolling accumulation of
+    volume based on Close price direction.
+    
+    Parameters:
+        `quotes` : Iterable[Quote]
+            Historical price quotes.
+
+        `sma_periods` : int, optional
+            Number of periods for an SMA of the OBV line.
+
+    Returns:
+        `OBVResults[OBVResult]`
+            OBVResults is list of OBVResult with providing useful helper methods.
+
+    See more:
+         - [OBV Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/Obv/#content)
+         - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
+    """
+    results = CsIndicator.GetObv[Quote](CsList(Quote, quotes), sma_periods)
+    return OBVResults(results, OBVResult)
+
+
+class OBVResult(ResultBase):
+    """
+    A wrapper class for a single unit of On-balance Volume (OBV) results.
+    """
+
+    @property
+    def obv(self) -> float:
+        return self._csdata.Obv
+
+    @obv.setter
+    def obv(self, value):
+        self._csdata.Obv = value
+
+    @property
+    def obv_sma(self) -> Optional[float]:
+        return self._csdata.ObvSma
+
+    @obv_sma.setter
+    def obv_sma(self, value):
+        self._csdata.ObvSma = value
+
+
+_T = TypeVar("_T", bound=OBVResult)
+class OBVResults(ToQuotesMixin, IndicatorResults[_T]):
+    """
+    A wrapper class for the list of On-balance Volume (OBV) results.
+    It is exactly same with built-in `list` except for that it provides
+    some useful helper methods written in CSharp implementation.
+    """
---
+++ tests/test_obv.py
@@ -0,0 +1,58 @@
+import pytest
+from stock_indicators import indicators
+
+class TestOBV:
+    def test_standard(self, quotes):
+        results = indicators.get_obv(quotes)
+        
+        assert 502 == len(results)
+        assert 502 == len(list(filter(lambda x: x.obv_sma is None, results)))
+        
+        r = results[249]
+        assert 1780918888 == r.obv
+        assert r.obv_sma is None
+        
+        r = results[501]
+        assert 539843504 == r.obv
+        assert r.obv_sma is None
+        
+    def test_with_sma(self, quotes):
+        results = indicators.get_obv(quotes, 20)
+        
+        assert 502 == len(results)
+        assert 482 == len(list(filter(lambda x: x.obv_sma, results)))
+        
+        r = results[501]
+        assert     539843504 == r.obv
+        assert 1016208844.40 == r.obv_sma
+        
+    def test_convert_to_quotes(self, quotes):
+        new_quotes = indicators.get_obv(quotes).to_quotes()
+        
+        assert 502 == len(new_quotes)
+        
+        q = new_quotes[249]
+        assert 1780918888 == q.close
+        
+        q = new_quotes[501]
+        assert 539843504 == q.close
+        
+    def test_bad_data(self, bad_quotes):
+        r = indicators.get_obv(bad_quotes)
+        assert 502 == len(r)
+        
+    def test_big_data(self, big_quotes):
+        r = indicators.get_obv(big_quotes)
+        assert 1246 == len(r)
+        
+    def test_no_quotes(self, quotes):
+        r = indicators.get_obv([])
+        assert 0 == len(r)
+        
+        r = indicators.get_obv(quotes[:1])
+        assert 1 == len(r)
+        
+    def test_exceptions(self, quotes):
+        from System import ArgumentOutOfRangeException
+        with pytest.raises(ArgumentOutOfRangeException):
+            indicators.get_obv(quotes, 0)
