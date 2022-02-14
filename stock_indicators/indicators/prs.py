0928eadb7627f82aa3b80532edf67b9018f2ad80
DongGeon Lee <secmatth1996@gmail.com>
2022-02-14 13:40:59 +0900

    feat: Add Price Relative Strength (#150)

---
+++ stock_indicators/indicators/prs.py
@@ -0,0 +1,81 @@
+from typing import Iterable, Optional, TypeVar
+from stock_indicators._cslib import CsIndicator
+from stock_indicators._cstypes import List as CsList
+from stock_indicators.indicators.common.results import IndicatorResults, ResultBase
+from stock_indicators.indicators.common.quote import Quote
+
+
+def get_prs(base_history: Iterable[Quote], eval_history: Iterable[Quote],
+            lookback_periods: Optional[int] = None, sma_periods: Optional[int] = None):
+    """Get PRS calculated.
+
+    Price Relative Strength (PRS), also called Comparative Relative Strength,
+    shows the ratio of two quote histories, based on Close price.
+    It is often used to compare against a market index or sector ETF.
+    When using the optional lookbackPeriods, this also return relative percent
+    change over the specified periods.
+
+    Parameters:
+        `base_history` : Iterable[Quote]
+            This is usually market index data,
+            but could be any baseline data that you might use for comparison.
+
+        `base_history` : Iterable[Quote]
+            Historical price quotes for evaluation.
+
+        `lookback_periods` : int, optional
+            Number of periods for % difference.
+
+        `sma_periods` : int, optional
+            Number of periods for a PRS SMA signal line.
+
+    Returns:
+        `PRSResults[PRSResult]`
+            PRSResults is list of PRSResult with providing useful helper methods.
+
+    See more:
+         - [PRS Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/Prs/#content)
+         - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
+    """
+    results = CsIndicator.GetPrs[Quote](CsList(Quote, base_history), CsList(Quote, eval_history),
+                                        lookback_periods, sma_periods)
+    return PRSResults(results, PRSResult)
+
+
+class PRSResult(ResultBase):
+    """
+    A wrapper class for a single unit of Price Relative Strength (PRS) results.
+    """
+
+    @property
+    def prs(self) -> Optional[float]:
+        return self._csdata.Prs
+
+    @prs.setter
+    def prs(self, value):
+        self._csdata.Prs = value
+
+    @property
+    def prs_sma(self) -> Optional[float]:
+        return self._csdata.PrsSma
+
+    @prs_sma.setter
+    def prs_sma(self, value):
+        self._csdata.PrsSma = value
+
+    @property
+    def prs_percent(self) -> Optional[float]:
+        return self._csdata.PrsPercent
+
+    @prs_percent.setter
+    def prs_percent(self, value):
+        self._csdata.PrsPercent = value
+
+
+_T = TypeVar("_T", bound=PRSResult)
+class PRSResults(IndicatorResults[_T]):
+    """
+    A wrapper class for the list of Price Relative Strength (PRS) results.
+    It is exactly same with built-in `list` except for that it provides
+    some useful helper methods written in CSharp implementation.
+    """
---
+++ tests/test_prs.py
@@ -0,0 +1,54 @@
+import pytest
+from stock_indicators import indicators
+
+class TestPRS:
+    def test_standard(self, quotes, other_quotes):
+        results = indicators.get_prs(quotes, other_quotes, 30, 10)
+        
+        assert 502 == len(results)
+        assert 502 == len(list(filter(lambda x: x.prs is not None, results)))
+        assert 493 == len(list(filter(lambda x: x.prs_sma is not None, results)))
+        
+        r = results[8]
+        assert 1.108340 == round(float(r.prs), 6)
+        assert r.prs_sma is None
+        assert r.prs_percent is None
+        
+        r = results[249]
+        assert  1.222373 == round(float(r.prs), 6)
+        assert  1.275808 == round(float(r.prs_sma), 6)
+        assert -0.023089 == round(float(r.prs_percent), 6) 
+        
+        r = results[501]
+        assert 1.356817 == round(float(r.prs), 6)
+        assert 1.343445 == round(float(r.prs_sma), 6)
+        assert 0.037082 == round(float(r.prs_percent), 6) 
+        
+    def test_bad_data(self, bad_quotes):
+        r = indicators.get_prs(bad_quotes, bad_quotes, 15, 4)
+        assert 502 == len(r)
+        
+    def test_no_quotes(self, quotes):
+        r = indicators.get_prs([], [])
+        assert 0 == len(r)
+        
+        r = indicators.get_prs(quotes[:1], quotes[:1])
+        assert 1 == len(r)
+        
+    def test_exceptions(self, quotes, other_quotes, mismatch_quotes):
+        from System import ArgumentOutOfRangeException
+        with pytest.raises(ArgumentOutOfRangeException):
+            indicators.get_prs(quotes, other_quotes, 0)
+            
+        with pytest.raises(ArgumentOutOfRangeException):
+            indicators.get_prs(quotes, other_quotes, 14, 0)
+
+        from Skender.Stock.Indicators import InvalidQuotesException
+        with pytest.raises(InvalidQuotesException):
+            indicators.get_prs(quotes, other_quotes[:13], 14)
+            
+        with pytest.raises(InvalidQuotesException):
+            indicators.get_prs(quotes, other_quotes[:300], 14)
+            
+        with pytest.raises(InvalidQuotesException):
+            indicators.get_prs(mismatch_quotes, other_quotes, 14)
