8240e3c1e949a672f140042d3ee895d83b9d5950
DongGeon Lee <secmatth1996@gmail.com>
2022-03-10 12:50:02 +0900

    feat: Add Marubozu and CandleStick wrappers (#174)

--- stock_indicators/indicators/common/enums.py
+++ stock_indicators/indicators/common/enums.py
@@ -1,4 +1,4 @@
-from stock_indicators._cslib import CsCandlePart
+from stock_indicators._cslib import CsCandlePart, CsSignal, CsEnum
 from stock_indicators.indicators.common._contrib.enum import ValueEnum
 
 class CandlePart(ValueEnum):
@@ -7,3 +7,14 @@ class CandlePart(ValueEnum):
     LOW = CsCandlePart.Low
     CLOSE = CsCandlePart.Close
     VOLUME = CsCandlePart.Volume
+
+
+class Signal(ValueEnum):
+    BULL_CONFIRMED = CsSignal.BullConfirmed
+    BULL_SIGNAL = CsSignal.BullSignal
+    BULL_BASIS = CsSignal.BullBasis
+    NEUTRAL = CsSignal.Neutral
+    NONE = CsEnum.Parse(CsSignal, "None")
+    BEAR_BASIS = CsSignal.BearBasis
+    BEAR_SIGNAL = CsSignal.BearSignal
+    BEAR_CONFIRMED = CsSignal.BearConfirmed
