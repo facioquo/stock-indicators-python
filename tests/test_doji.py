b5eb4552b4c3f05ef2ff5a0dae9b90a5a1b0ded5
Dong-Geon Lee <secmatth1996@gmail.com>
2022-03-10 15:59:47 +0900

    Add Doji

---
+++ tests/test_doji.py
@@ -0,0 +1,61 @@
+import pytest
+from stock_indicators import indicators
+from stock_indicators.indicators.common.enums import Signal
+
+class TestDoji:
+    def test_standard(self, quotes):
+        results = indicators.get_doji(quotes, 0.001)
+        
+        assert 502 == len(results)
+        assert 112 == len(list(filter(lambda x: x.signal != Signal.NONE, results)))
+        
+        r = results[1]
+        assert r.price is None
+        assert r.signal == 0
+        
+        r = results[23]
+        assert 216.28 == round(float(r.price), 2)
+        assert Signal.NEUTRAL == r.signal
+        
+        r = results[46]
+        assert r.price is None
+        assert Signal.NONE == r.signal
+        
+        r = results[34]
+        assert r.price is None
+        assert Signal.NONE == r.signal
+        
+        r = results[392]
+        assert r.price is None
+        assert Signal.NONE == r.signal
+        
+        r = results[451]
+        assert 273.64 == round(float(r.price), 2)
+        assert 1 == r.signal
+        
+        r = results[477]
+        assert 256.86 == round(float(r.price), 2)
+        assert Signal.NEUTRAL == r.signal
+        
+    def test_bad_data(self, bad_quotes):
+        r = indicators.get_doji(bad_quotes)
+        assert 502 == len(r)
+        
+    def test_no_quotes(self, quotes):
+        r = indicators.get_doji([])
+        assert 0 == len(r)
+        
+        r = indicators.get_doji(quotes[:1])
+        assert 1 == len(r)
+        
+    def test_condense(self, quotes):
+        r = indicators.get_doji(quotes, 0.001).condense()
+        assert 112 == len(r)
+ 
+    def test_exceptions(self, quotes):
+        from System import ArgumentOutOfRangeException
+        with pytest.raises(ArgumentOutOfRangeException):
+            indicators.get_doji(quotes, -0.00001)
+        
+        with pytest.raises(ArgumentOutOfRangeException):
+            indicators.get_doji(quotes, 0.0050001)
