0598ece7d5123a745bbd605e3cdcfadc9a0bca72
Dong-Geon Lee <secmatth1996@gmail.com>
2022-03-26 18:11:08 +0900

    Add Gator Oscillator

---
+++ stock_indicators/indicators/gator.py
@@ -0,0 +1,89 @@
+from typing import Iterable, Optional, TypeVar, overload
+
+from stock_indicators._cslib import CsIndicator
+from stock_indicators._cstypes import List as CsList
+from stock_indicators.indicators.alligator import AlligatorResult
+from stock_indicators.indicators.common.helpers import RemoveWarmupMixin
+from stock_indicators.indicators.common.results import IndicatorResults, ResultBase
+from stock_indicators.indicators.common.quote import Quote
+
+@overload
+def get_gator(quotes: Iterable[Quote]) -> "GatorResults[GatorResult]": ...
+@overload
+def get_gator(quotes: Iterable[AlligatorResult]) -> "GatorResults[GatorResult]": ...
+def get_gator(quotes):
+    """Get Gator Oscillator calculated.
+
+    Gator Oscillator is an expanded view of Williams Alligator.
+
+    Parameters:
+        `quotes` : Iterable[Quote] or Iterable[AlligatorResult]
+            Historical price quotes.
+
+    Returns:
+        `GatorResults[GatorResult]`
+            GatorResults is list of GatorResult with providing useful helper methods.
+
+    See more:
+         - [Gator Oscillator Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/Gator/#content)
+         - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
+    """
+    if not quotes or type(quotes[0]) == Quote:
+        results = CsIndicator.GetGator[Quote](CsList(Quote, quotes))
+    else:
+        # Get C# objects.
+        if isinstance(quotes, IndicatorResults):
+            quotes.reload()
+            cs_results = quotes._csdata
+        else:
+            cs_results = [ q._csdata for q in quotes ]
+            
+        results = CsIndicator.GetGator(CsList(type(cs_results[0]), cs_results))
+    return GatorResults(results, GatorResult)
+
+
+class GatorResult(ResultBase):
+    """
+    A wrapper class for a single unit of Gator Oscillator results.
+    """
+
+    @property
+    def upper(self) -> Optional[float]:
+        return self._csdata.Upper
+
+    @upper.setter
+    def upper(self, value):
+        self._csdata.Upper = value
+
+    @property
+    def lower(self) -> Optional[float]:
+        return self._csdata.Lower
+
+    @lower.setter
+    def lower(self, value):
+        self._csdata.Lower = value
+
+    @property
+    def is_upper_expanding(self) -> Optional[bool]:
+        return self._csdata.UpperIsExpanding
+
+    @is_upper_expanding.setter
+    def is_upper_expanding(self, value):
+        self._csdata.UpperIsExpanding = value
+
+    @property
+    def is_lower_expanding(self) -> Optional[bool]:
+        return self._csdata.LowerIsExpanding
+
+    @is_lower_expanding.setter
+    def is_lower_expanding(self, value):
+        self._csdata.LowerIsExpanding = value
+
+
+_T = TypeVar("_T", bound=GatorResult)
+class GatorResults(RemoveWarmupMixin, IndicatorResults[_T]):
+    """
+    A wrapper class for the list of Gator Oscillator results.
+    It is exactly same with built-in `list` except for that it provides
+    some useful helper methods written in C# implementation.
+    """
---
+++ tests/test_gator.py
@@ -0,0 +1,94 @@
+from stock_indicators import indicators
+
+class TestFractal:
+    def test_standard(self, quotes):
+        results = indicators.get_gator(quotes)
+
+        assert 502 == len(results)
+        assert 482 == len(list(filter(lambda x: x.upper is not None, results)))
+        assert 490 == len(list(filter(lambda x: x.lower is not None, results)))
+        assert 481 == len(list(filter(lambda x: x.is_upper_expanding is not None, results)))
+        assert 489 == len(list(filter(lambda x: x.is_lower_expanding is not None, results)))
+
+        r = results[11]
+        assert r.upper is None
+        assert r.lower is None
+        assert r.is_upper_expanding is None
+        assert r.is_lower_expanding is None
+
+        r = results[12]
+        assert r.upper is None
+        assert -0.1402 == round(float(r.lower), 4)
+        assert r.is_upper_expanding is None
+        assert r.is_lower_expanding is None
+        
+        r = results[13]
+        assert r.upper is None
+        assert -0.0406 == round(float(r.lower), 4)
+        assert r.is_upper_expanding is None
+        assert r.is_lower_expanding == False
+        
+        r = results[19]
+        assert r.upper is None
+        assert -1.0018 == round(float(r.lower), 4)
+        assert r.is_upper_expanding is None
+        assert r.is_lower_expanding == True
+        
+        r = results[20]
+        assert  0.4004 == round(float(r.upper), 4)
+        assert -1.0130 == round(float(r.lower), 4)
+        assert r.is_upper_expanding is None
+        assert r.is_lower_expanding == True
+        
+        r = results[21]
+        assert  0.7298 == round(float(r.upper), 4)
+        assert -0.6072 == round(float(r.lower), 4)
+        assert r.is_upper_expanding == True
+        assert r.is_lower_expanding == False
+
+        r = results[99]
+        assert  0.5159 == round(float(r.upper), 4)
+        assert -0.2320 == round(float(r.lower), 4)
+        assert r.is_upper_expanding == False
+        assert r.is_lower_expanding == True
+        
+        r = results[249]
+        assert  3.1317 == round(float(r.upper), 4)
+        assert -1.8058 == round(float(r.lower), 4)
+        assert r.is_upper_expanding == True
+        assert r.is_lower_expanding == False
+        
+        r = results[501]
+        assert  7.4538 == round(float(r.upper), 4)
+        assert -9.2399 == round(float(r.lower), 4)
+        assert r.is_upper_expanding == True
+        assert r.is_lower_expanding == True
+        
+    def test_gator_with_alligator(self, quotes):
+        alligator_results = indicators.get_alligator(quotes)
+        alligator_results.done()
+        
+        results = indicators.get_gator(alligator_results)
+        assert 502 == len(results)
+
+    def test_bad_data(self, bad_quotes):
+        r = indicators.get_gator(bad_quotes)
+        assert 502 == len(r)
+        
+    def test_no_quotes(self, quotes):
+        r = indicators.get_gator([])
+        assert 0 == len(r)
+        
+        r = indicators.get_gator(quotes[:1])
+        assert 1 == len(r)
+        
+    def test_removed(self, quotes):
+        results = indicators.get_gator(quotes).remove_warmup_periods()
+        
+        assert 502 - 150 == len(results)
+        
+        last = results.pop()
+        assert  7.4538 == round(float(last.upper), 4)
+        assert -9.2399 == round(float(last.lower), 4)
+        assert last.is_upper_expanding == True
+        assert last.is_lower_expanding == True
