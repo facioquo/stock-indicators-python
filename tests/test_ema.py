28b66dd8b4d3add5733d8af24c1f36683f00acd8
DongGeon Lee <secmatth1996@gmail.com>
2022-04-16 14:52:43 +0900

    feat: Support CandlePart for EMA (#219)

--- tests/test_ema.py
+++ tests/test_ema.py
@@ -1,5 +1,6 @@
 import pytest
 from stock_indicators import indicators
+from stock_indicators.indicators.common.enums import CandlePart
 
 class TestEMA:
     def test_standard(self, quotes):
@@ -18,13 +19,31 @@ class TestEMA:
         assert 249.3519 == round(float(r.ema), 4)
 
     def test_custom(self, quotes):
-        # TODO: Support CandlePart.
-        pass
+        results = indicators.get_ema(quotes, 20, CandlePart.OPEN)
+        
+        assert 502 == len(results)
+        assert 483 == len(list(filter(lambda x: x.ema is not None, results)))
+
+        r = results[29]
+        assert 216.2643 == round(float(r.ema), 4)
+
+        r = results[249]
+        assert 255.4875 == round(float(r.ema), 4)
+
+        r = results[501]
+        assert 249.9157 == round(float(r.ema), 4)
 
     def test_bad_data(self, bad_quotes):
         r = indicators.get_ema(bad_quotes, 15)
 
         assert 502 == len(r)
+        
+    def test_no_quotes(self, quotes):
+        r = indicators.get_ema([], 10)
+        assert 0 == len(r)
+        
+        r = indicators.get_ema(quotes[:1], 10)
+        assert 1 == len(r)
 
     def test_removed(self, quotes):
         results = indicators.get_ema(quotes, 20).remove_warmup_periods()
