779e9258e33bcb03392ae43d988dac4fca087c94
Dong-Geon Lee <secmatth1996@gmail.com>
2022-05-29 19:45:39 +0900

    Revert removing to_quotes() and mark it deprecated

--- stock_indicators/indicators/rsi.py
+++ stock_indicators/indicators/rsi.py
@@ -2,7 +2,7 @@ from typing import Iterable, Optional, TypeVar
 
 from stock_indicators._cslib import CsIndicator
 from stock_indicators._cstypes import List as CsList
-from stock_indicators.indicators.common.helpers import RemoveWarmupMixin
+from stock_indicators.indicators.common.helpers import RemoveWarmupMixin, ToQuotesMixin
 from stock_indicators.indicators.common.results import IndicatorResults, ResultBase
 from stock_indicators.indicators.common.quote import Quote
 
@@ -47,7 +47,7 @@ class RSIResult(ResultBase):
 
 
 _T = TypeVar("_T", bound=RSIResult)
-class RSIResults(RemoveWarmupMixin, IndicatorResults[_T]):
+class RSIResults(ToQuotesMixin, RemoveWarmupMixin, IndicatorResults[_T]):
     """
     A wrapper class for the list of RSI(Relative Strength Index) results.
     It is exactly same with built-in `list` except for that it provides
