e3840824fd29426fc52b859ee8c8c4e2d67aa137
Dong-Geon Lee <secmatth1996@gmail.com>
2022-05-30 16:36:35 +0900

    Add deprecated docstring

--- stock_indicators/indicators/common/helpers.py
+++ stock_indicators/indicators/common/helpers.py
@@ -28,8 +28,11 @@ class ToQuotesMixin:
     """Mixin for to_quotes()."""
     @IndicatorResults._verify_data
     def to_quotes(self) -> List[Quote]:
-        """Convert indicator results into historical quotes."""
+        """Deprecated. 
+        Convert indicator results into historical quotes."""
+        
         warn('The .to_quotes() method is deprecated.', DeprecationWarning, stacklevel=2)
+        
         quotes = CsIndicator.ConvertToQuotes(CsList(self._get_csdata_type(), self._csdata))
 
         return [ Quote.from_csquote(q) for q in quotes ]
