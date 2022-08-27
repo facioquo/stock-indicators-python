74b56c565e475daaef3874939c8fd4eac9bf7ad4
DongGeon Lee <secmatth1996@gmail.com>
2022-08-27 13:36:57 +0900

    Add more params for Parabolic SAR

--- tests/test_parabolic_sar.py
+++ tests/test_parabolic_sar.py
@@ -24,6 +24,37 @@ class TestParabolicSAR:
         assert 229.7662 == round(float(r.sar), 4)
         assert False == r.is_reversal
         
+    def test_extended(self, quotes):
+        accleration_step = 0.02
+        max_acceleration_factor = 0.2
+        initial_factor = 0.01
+
+        results = indicators.get_parabolic_sar(quotes, accleration_step, 
+                                                max_acceleration_factor, initial_factor)
+
+        assert 502 == len(results)
+        assert 488 == len(list(filter(lambda x: x.sar is not None, results)))
+
+        r = results[14]
+        assert 212.8300 == round(float(r.sar), 4)
+        assert True == r.is_reversal
+        
+        r = results[16]
+        assert 212.9518 == round(float(r.sar), 4)
+        assert False == r.is_reversal
+        
+        r = results[94]
+        assert 228.3600 == round(float(r.sar), 4)
+        assert False == r.is_reversal
+
+        r = results[486]
+        assert 273.4148 == round(float(r.sar), 4)
+        assert False == r.is_reversal
+        
+        r = results[501]
+        assert 246.73 == round(float(r.sar), 4)
+        assert False == r.is_reversal
+
     def test_bad_data(self, bad_quotes):
         r = indicators.get_parabolic_sar(bad_quotes)
         assert 502 == len(r)
