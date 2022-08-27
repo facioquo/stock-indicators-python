0735e49d1f89ea6928add516ed49f526af429cec
DongGeon Lee <secmatth1996@gmail.com>
2022-08-27 16:57:12 +0900

    Add tests for type comparing

--- tests/common/test_type_compatibility.py
+++ tests/common/test_type_compatibility.py
@@ -1,6 +1,8 @@
 
+from enum import Enum, IntEnum
 from stock_indicators._cslib import CsQuote, CsCandleProperties
 from stock_indicators.indicators.common.candles import CandleProperties
+from stock_indicators.indicators.common.enums import PivotPointType
 from stock_indicators.indicators.common.quote import Quote
 
 class TestTypeCompat:
@@ -12,3 +14,12 @@ class TestTypeCompat:
         assert issubclass(CandleProperties, Quote)
         assert issubclass(CandleProperties, CsQuote)
         assert issubclass(CandleProperties, CsCandleProperties)
+
+    def test_cs_compatible_enum(self):
+        assert isinstance(PivotPointType.STANDARD, Enum)
+        assert isinstance(PivotPointType.STANDARD, IntEnum)
+        assert isinstance(PivotPointType.STANDARD.value, int)
+
+        assert int(PivotPointType.STANDARD) == 0
+        assert PivotPointType.STANDARD == 0
+        assert PivotPointType.STANDARD == PivotPointType.STANDARD
