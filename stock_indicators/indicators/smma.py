4110db4a32c832553b5262cd6aa4f56a3c44d9dc
Dong-Geon Lee <secmatth1996@gmail.com>
2022-10-02 22:17:36 +0900

    Change SMMA rtn value type

--- stock_indicators/indicators/smma.py
+++ stock_indicators/indicators/smma.py
@@ -1,10 +1,7 @@
-from decimal import Decimal
 from typing import Iterable, Optional, TypeVar
 
 from stock_indicators._cslib import CsIndicator
 from stock_indicators._cstypes import List as CsList
-from stock_indicators._cstypes import Decimal as CsDecimal
-from stock_indicators._cstypes import to_pydecimal
 from stock_indicators.indicators.common.helpers import RemoveWarmupMixin
 from stock_indicators.indicators.common.results import IndicatorResults, ResultBase
 from stock_indicators.indicators.common.quote import Quote
@@ -41,12 +38,12 @@ class SMMAResult(ResultBase):
     """
 
     @property
-    def smma(self) -> Optional[Decimal]:
-        return to_pydecimal(self._csdata.Smma)
+    def smma(self) -> Optional[float]:
+        return self._csdata.Smma
 
     @smma.setter
     def smma(self, value):
-        self._csdata.Smma = CsDecimal(value)
+        self._csdata.Smma = value
 
 
 _T = TypeVar("_T", bound=SMMAResult)
