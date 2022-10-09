b4b364c3016d56fa6436ff7d97138fc627bf8deb
DongGeon Lee <secmatth1996@gmail.com>
2022-10-10 03:44:14 +0900

    fix: Fix wrapping CandleProperties (#278)

--- stock_indicators/indicators/common/_contrib/type_resolver.py
+++ stock_indicators/indicators/common/_contrib/type_resolver.py
@@ -1,5 +1,5 @@
 from typing import Type, TypeVar, cast
 
 _T = TypeVar("_T")
-def generate_cs_inherited_class(child: Type[_T], cs_parent: Type):
-    return cast(Type[_T] , type("_Wrapper", (cs_parent, ), dict(child.__dict__)))
+def generate_cs_inherited_class(child: Type[_T], cs_parent: Type, class_name="_Wrapper"):
+    return cast(Type[_T], type(class_name, (cs_parent, ), {attr: getattr(child, attr) for attr in  dir(child)}))
