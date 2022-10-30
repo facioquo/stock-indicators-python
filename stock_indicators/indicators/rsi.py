fc19707cf0087fc007efb21f46c57f6400fbcecd
Dong-Geon Lee <secmatth1996@gmail.com>
2022-10-31 03:24:56 +0900

    Use Indicator class for RSI

--- stock_indicators/indicators/rsi.py
+++ stock_indicators/indicators/rsi.py
@@ -1,44 +1,11 @@
 from typing import Iterable, Optional, TypeVar
 
 from stock_indicators._cslib import CsIndicator
-from stock_indicators.indicators.common.chain import chainable
+from stock_indicators.indicators.common.chain import calculate_indicator
 from stock_indicators.indicators.common.helpers import RemoveWarmupMixin
+from stock_indicators.indicators.common.indicator import Indicator
 from stock_indicators.indicators.common.results import IndicatorResults, ResultBase
 from stock_indicators.indicators.common.quote import Quote
-
-
-def _wrap_results(results):
-    return RSIResults(results, RSIResult)
-
-def _calculate(indicator_params, is_chaining):
-    if is_chaining:
-        return CsIndicator.GetRsi(*indicator_params)
-    else:
-        return CsIndicator.GetRsi[Quote](*indicator_params)
-
-@chainable(is_chainable=True, calc_func=_calculate, wrap_func=_wrap_results)
-def get_rsi(quotes: Iterable[Quote], lookback_periods: int = 14) -> "RSIResults[RSIResult]":
-    """Get RSI calculated.
-
-    Relative Strength Index (RSI) measures strength of the winning/losing streak over N lookback periods
-    on a scale of 0 to 100, to depict overbought and oversold conditions.
-
-    Parameters:
-        `quotes` : Iterable[Quote]
-            Historical price quotes.
-
-        `lookback_periods` : int, defaults 14
-            Number of periods in the lookback window.
-
-    Returns:
-        `RSIResults[RSIResult]`
-            RSIResults is list of RSIResult with providing useful helper methods.
-
-    See more:
-         - [RSI Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/Rsi/#content)
-         - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
-    """
-    return (quotes, lookback_periods)
 
 
 class RSIResult(ResultBase):
@@ -62,3 +29,39 @@ class RSIResults(RemoveWarmupMixin, IndicatorResults[_T]):
     It is exactly same with built-in `list` except for that it provides
     some useful helper methods written in CSharp implementation.
     """
+
+class RSI(Indicator):
+    is_chainable = True
+    is_chainee = True
+    is_chainor = True
+    
+    indicator_method = CsIndicator.GetRsi[Quote]
+    chaining_method =CsIndicator.GetRsi
+    
+    list_wrap_class = RSIResults
+    unit_wrap_class = RSIResult
+
+
+@calculate_indicator(indicator=RSI())
+def get_rsi(quotes: Iterable[Quote], lookback_periods: int = 14) -> "RSIResults[RSIResult]":
+    """Get RSI calculated.
+
+    Relative Strength Index (RSI) measures strength of the winning/losing streak over N lookback periods
+    on a scale of 0 to 100, to depict overbought and oversold conditions.
+
+    Parameters:
+        `quotes` : Iterable[Quote]
+            Historical price quotes.
+
+        `lookback_periods` : int, defaults 14
+            Number of periods in the lookback window.
+
+    Returns:
+        `RSIResults[RSIResult]`
+            RSIResults is list of RSIResult with providing useful helper methods.
+
+    See more:
+         - [RSI Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/Rsi/#content)
+         - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
+    """
+    return (quotes, lookback_periods)
