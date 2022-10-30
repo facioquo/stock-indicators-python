dbeebe337bd86cf2394b37397fa5e0efe33facf5
Dong-Geon Lee <secmatth1996@gmail.com>
2022-10-31 03:22:14 +0900

    Add base indicator class

--- stock_indicators/indicators/common/indicator.py
+++ stock_indicators/indicators/common/indicator.py
@@ -4,25 +4,52 @@ from stock_indicators._cstypes import List as CsList
 from stock_indicators.indicators.common.quote import Quote
 
 class Indicator(ABC):
-    is_chainable = False
-    is_chainee = False
-    is_chainor = False
-    
-    indicator_method = None
-    chaining_method = None
-    
-    def __call__(self, quotes, *params):
-        return self._wrap_results(self._calculate(CsList(Quote, quotes), *params))
+    @property
+    @abstractmethod
+    def is_chainable(self):
+        pass
 
-    def _calculate(self, *params, is_chaining=False):
+    @property
+    @abstractmethod
+    def is_chainee(self):
+        pass
+
+    @property
+    @abstractmethod
+    def is_chainor(self):
+        pass
+
+    @property
+    @abstractmethod
+    def indicator_method(self):
+        pass
+
+    @property
+    @abstractmethod
+    def chaining_method(self):
+        pass
+
+    @property
+    @abstractmethod
+    def list_wrap_class(self):
+        pass
+
+    @property
+    @abstractmethod
+    def unit_wrap_class(self):
+        pass
+
+    def __call__(self, quotes, *params):
+        return self.wrap_results(self.calculate((CsList(Quote, quotes), *params)))
+
+    def calculate(self, params, is_chaining = False):
         if is_chaining:
             if self.is_chainable:
                 return self.chaining_method(*params)
             else:
-                raise RuntimeError()
+                raise RuntimeError(f"{self.__class__.__name__} cannot be used in chaining.")
         else:
             return self.indicator_method(*params)
 
-    @abstractmethod
-    def _wrap_results(self, results):
-        pass
+    def wrap_results(self, results):
+        return self.list_wrap_class(results, self.unit_wrap_class)
