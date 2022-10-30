a7a7829793d2695dc4d5f4a4910f0cffb8ee204e
Dong-Geon Lee <secmatth1996@gmail.com>
2022-10-31 03:24:48 +0900

    Use Indicator class for Alligator

--- stock_indicators/indicators/alligator.py
+++ stock_indicators/indicators/alligator.py
@@ -1,40 +1,11 @@
-from typing import Callable, Iterable, Optional, TypeVar, overload
+from typing import Iterable, Optional, TypeVar
 
 from stock_indicators._cslib import CsIndicator
-from stock_indicators.indicators.common.chain import chainable
+from stock_indicators.indicators.common.chain import calculate_indicator
 from stock_indicators.indicators.common.helpers import RemoveWarmupMixin
 from stock_indicators.indicators.common.indicator import Indicator
 from stock_indicators.indicators.common.results import IndicatorResults, ResultBase
 from stock_indicators.indicators.common.quote import Quote
-
-
-class Alligator(Indicator):
-    is_chainable = True
-    is_chainee = True
-    is_chainor = True
-    
-    indicator_method = CsIndicator.GetAlligator[Quote]
-    chaining_method = CsIndicator.GetAlligator
-    
-    def __call__(self, quotes: Iterable[Quote],
-                  jaw_periods: int = 13, jaw_offset: int = 8,
-                  teeth_periods: int = 8, teeth_offset: int = 5,
-                  lips_periods: int = 5, lips_offset: int = 3) -> "AlligatorResults[AlligatorResult]":
-        """Get Williams Alligator calculated.
-
-        Williams Alligator is an indicator that transposes multiple moving averages,
-        showing chart patterns that creator Bill Williams compared to an alligator's
-        feeding habits when describing market movement.
-        """
-
-        return super().__call__(quotes, jaw_periods,
-                                jaw_offset, teeth_periods,
-                                teeth_offset, lips_periods, lips_offset)
-
-    def _wrap_results(self, results):
-        return AlligatorResults(results, AlligatorResult)
-
-get_alligator = Alligator().__call__
 
 
 class AlligatorResult(ResultBase):
@@ -74,3 +45,59 @@ class AlligatorResults(RemoveWarmupMixin, IndicatorResults[_T]):
     It is exactly same with built-in `list` except for that it provides
     some useful helper methods written in C# implementation.
     """
+
+
+class Alligator(Indicator):
+    is_chainable = True
+    is_chainee = True
+    is_chainor = True
+    
+    indicator_method = CsIndicator.GetAlligator[Quote]
+    chaining_method = CsIndicator.GetAlligator
+    
+    list_wrap_class = AlligatorResults
+    unit_wrap_class = AlligatorResult
+
+
+@calculate_indicator(indicator=Alligator())
+def get_alligator(quotes: Iterable[Quote],
+                  jaw_periods: int = 13, jaw_offset: int = 8,
+                  teeth_periods: int = 8, teeth_offset: int = 5,
+                  lips_periods: int = 5, lips_offset: int = 3):
+    """Get Williams Alligator calculated.
+
+    Williams Alligator is an indicator that transposes multiple moving averages,
+    showing chart patterns that creator Bill Williams compared to an alligator's
+    feeding habits when describing market movement.
+
+    Parameters:
+        `quotes` : Iterable[Quote]
+            Historical price quotes.
+
+        `jaw_periods` : int, defaults 13
+            Lookback periods for the Jaw line
+
+        `jaw_offset` : int, defaults 8
+            Offset periods for the Jaw line.
+
+        `teeth_periods` : int, defaults 8
+            Lookback periods for the Teeth line.
+
+        `teeth_offset` : int, defaults 5
+            Offset periods for the Teeth line.
+
+        `lips_periods` : int, defaults 5
+            Lookback periods for the Lips line.
+
+        `lips_offset` : int, defaults 3
+            Offset periods for the Lips line.
+
+    Returns:
+        `AlligatorResults[AlligatorResult]`
+            AlligatorResults is list of AlligatorResult with providing useful helper methods.
+
+    See more:
+         - [Williams Alligator Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/Alligator/#content)
+         - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
+    """
+    return (quotes, jaw_periods, jaw_offset, teeth_periods, teeth_offset, lips_periods, lips_offset)
