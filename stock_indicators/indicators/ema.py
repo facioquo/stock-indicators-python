0bdb4c24a7ec83c02aed9b0ed59814d32e28a08f
Dong-Geon Lee <secmatth1996@gmail.com>
2022-11-08 01:02:50 +0900

    Use Indicator class for EMA

--- stock_indicators/indicators/ema.py
+++ stock_indicators/indicators/ema.py
@@ -1,39 +1,10 @@
 from typing import Iterable, Optional, TypeVar
 
 from stock_indicators._cslib import CsIndicator
-from stock_indicators.indicators.common.enums import CandlePart
 from stock_indicators.indicators.common.helpers import RemoveWarmupMixin
+from stock_indicators.indicators.common.indicator import Indicator, calculate_indicator
 from stock_indicators.indicators.common.results import IndicatorResults, ResultBase
 from stock_indicators.indicators.common.quote import Quote
-
-
-def get_ema(quotes: Iterable[Quote], lookback_periods: int,
-            candle_part: CandlePart = CandlePart.CLOSE):
-    """Get EMA calculated.
-
-    Exponential Moving Average (EMA) of the Close price.
-
-    Parameters:
-        `quotes` : Iterable[Quote]
-            Historical price quotes.
-
-        `lookback_periods` : int
-            Number of periods in the lookback window.
-
-        `candle_part` : CandlePart, defaults CandlePart.CLOSE
-            Selected OHLCV part.
-
-    Returns:
-        `EMAResults[EMAResult]`
-            EMAResults is list of EMAResult with providing useful helper methods.
-
-    See more:
-         - [EMA Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/Ema/#content)
-         - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
-    """
-    quotes = Quote.use(quotes, candle_part) # Error occurs if not assigned to local var.
-    ema_list = CsIndicator.GetEma(quotes, lookback_periods)
-    return EMAResults(ema_list, EMAResult)
 
 
 class EMAResult(ResultBase):
@@ -57,3 +28,41 @@ class EMAResults(RemoveWarmupMixin, IndicatorResults[_T]):
     It is exactly same with built-in `list` except for that it provides
     some useful helper methods written in CSharp implementation.
     """
+
+
+class EMA(Indicator):
+    is_chainee = True
+    is_chainor = True
+    
+    indicator_method = CsIndicator.GetEma[Quote]
+    chaining_method = CsIndicator.GetEma
+    
+    list_wrap_class = EMAResults
+    unit_wrap_class = EMAResult
+
+
+@calculate_indicator(indicator=EMA())
+def get_ema(quotes: Iterable[Quote], lookback_periods: int) -> EMAResults[EMAResult]:
+    """Get EMA calculated.
+
+    Exponential Moving Average (EMA) of the Close price.
+
+    Parameters:
+        `quotes` : Iterable[Quote]
+            Historical price quotes.
+
+        `lookback_periods` : int
+            Number of periods in the lookback window.
+
+        `candle_part` : CandlePart, defaults CandlePart.CLOSE
+            Selected OHLCV part.
+
+    Returns:
+        `EMAResults[EMAResult]`
+            EMAResults is list of EMAResult with providing useful helper methods.
+
+    See more:
+         - [EMA Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/Ema/#content)
+         - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
+    """
+    return (quotes, lookback_periods)
