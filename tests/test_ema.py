76a62bf7a8329b3df68fae62b5ac077e228e0ca9
Dong-Geon Lee <secmatth1996@gmail.com>
2022-11-08 01:15:47 +0900

    Add more chaining tests for EMA

--- tests/test_ema.py
+++ tests/test_ema.py
@@ -3,6 +3,7 @@ from stock_indicators import indicators
 from stock_indicators.indicators.common.chain import IndicatorChain
 from stock_indicators.indicators.common.enums import CandlePart
 from stock_indicators.indicators.ema import get_ema
+from stock_indicators.indicators.rsi import get_rsi
 from stock_indicators.indicators.sma import get_sma
 
 class TestEMA:
@@ -22,7 +23,7 @@ class TestEMA:
         assert 249.3519 == round(float(r.ema), 4)
 
     def test_custom(self, quotes):
-        results = indicators.get_ema(quotes, 20, CandlePart.OPEN)
+        results = IndicatorChain.use_quotes(quotes, CandlePart.OPEN).add(get_ema, 20).calc()
         
         assert 502 == len(results)
         assert 483 == len(list(filter(lambda x: x.ema is not None, results)))
@@ -53,7 +54,25 @@ class TestEMA:
             
         assert 502 == len(results)
         assert 474 == len(list(filter(lambda x: x.sma is not None, results)))
+        
+    def test_chaining(self, quotes):
+        results = IndicatorChain.use_quotes(quotes).add(get_rsi, 14).add(get_ema, 20).calc()
+        
+        assert 502 == len(results)
+        assert 469 == len(list(filter(lambda x: x.ema is not None, results)))
+                
+        r = results[32]
+        assert r.ema is None
 
+        r = results[33]
+        assert 67.4565 == round(float(r.ema), 4)
+
+        r = results[249]
+        assert 70.4659 == round(float(r.ema), 4)
+
+        r = results[501]
+        assert 37.0728 == round(float(r.ema), 4)
+        
     def test_bad_data(self, bad_quotes):
         r = indicators.get_ema(bad_quotes, 15)
 
