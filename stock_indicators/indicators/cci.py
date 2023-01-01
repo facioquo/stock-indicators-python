8f937173c26a1597dec1757f80fff164ae4438ad
Dong-Geon Lee <secmatth1996@gmail.com>
2023-01-01 14:49:21 +0900

    Use Indicator class for CCI

--- stock_indicators/indicators/cci.py
+++ stock_indicators/indicators/cci.py
@@ -1,35 +1,10 @@
 from typing import Iterable, Optional, TypeVar
 
 from stock_indicators._cslib import CsIndicator
-from stock_indicators._cstypes import List as CsList
 from stock_indicators.indicators.common.helpers import RemoveWarmupMixin
+from stock_indicators.indicators.common.indicator import Indicator, calculate_indicator
 from stock_indicators.indicators.common.results import IndicatorResults, ResultBase
 from stock_indicators.indicators.common.quote import Quote
-
-
-def get_cci(quotes: Iterable[Quote], lookback_periods: int = 20):
-    """Get CCI calculated.
-
-    Commodity Channel Index (CCI) is an oscillator depicting deviation
-    from typical price range, often used to identify cyclical trends.
-
-    Parameters:
-        `quotes` : Iterable[Quote]
-            Historical price quotes.
-
-        `lookback_periods` : int, defaults 20
-            Number of periods in the lookback window.
-
-    Returns:
-        `CCIResults[CCIResult]`
-            CCIResults is list of CCIResult with providing useful helper methods.
-
-    See more:
-         - [CCI Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/Cci/#content)
-         - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
-    """
-    results = CsIndicator.GetCci[Quote](CsList(Quote, quotes), lookback_periods)
-    return CCIResults(results, CCIResult)
 
 
 class CCIResult(ResultBase):
@@ -53,3 +28,39 @@ class CCIResults(RemoveWarmupMixin, IndicatorResults[_T]):
     It is exactly same with built-in `list` except for that it provides
     some useful helper methods written in CSharp implementation.
     """
+
+
+class CCI(Indicator):
+    is_chainee = False
+    is_chainor = True
+
+    indicator_method = CsIndicator.GetCci[Quote]
+    chaining_method = None
+
+    list_wrap_class = CCIResults
+    unit_wrap_class = CCIResult
+
+
+@calculate_indicator(indicator=CCI())
+def get_cci(quotes: Iterable[Quote], lookback_periods: int = 20) -> CCIResults[CCIResult]:
+    """Get CCI calculated.
+
+    Commodity Channel Index (CCI) is an oscillator depicting deviation
+    from typical price range, often used to identify cyclical trends.
+
+    Parameters:
+        `quotes` : Iterable[Quote]
+            Historical price quotes.
+
+        `lookback_periods` : int, defaults 20
+            Number of periods in the lookback window.
+
+    Returns:
+        `CCIResults[CCIResult]`
+            CCIResults is list of CCIResult with providing useful helper methods.
+
+    See more:
+         - [CCI Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/Cci/#content)
+         - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
+    """
+    return (quotes, lookback_periods)
--- tests/test_cci.py
+++ tests/test_cci.py
@@ -1,5 +1,6 @@
 import pytest
 from stock_indicators import indicators
+from stock_indicators.indicators.common.chain import IndicatorChain
 
 class TestCCI:
     def test_standard(self, quotes):
@@ -10,7 +11,23 @@ class TestCCI:
         
         r = results[501]
         assert -52.9946 == round(float(r.cci), 4)
-        
+
+    def test_chainor(self, quotes):
+        results = IndicatorChain.use_quotes(quotes)\
+            .add(indicators.get_cci, 20)\
+            .add(indicators.get_sma, 10)\
+            .calc()
+
+        assert 502 == len(results)
+        assert 474 == len(list(filter(lambda x: x.sma is not None, results)))
+
+    def test_chainee(self, quotes):
+        with pytest.raises(ValueError):
+            results = IndicatorChain.use_quotes(quotes)\
+            .add(indicators.get_sma)\
+            .add(indicators.get_cci)\
+            .calc()
+
     def test_bad_data(self, bad_quotes):
         r = indicators.get_cci(bad_quotes, 15)
         assert 502 == len(r)
