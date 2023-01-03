6c1b9300c87fdf19c4c4150a234dc66b0922c4db
Dong-Geon Lee <secmatth1996@gmail.com>
2023-01-03 23:18:47 +0900

    Use Indicator class for Chandelier Exit

--- stock_indicators/indicators/chandelier.py
+++ stock_indicators/indicators/chandelier.py
@@ -1,15 +1,50 @@
 from typing import Iterable, Optional, TypeVar
 
 from stock_indicators._cslib import CsIndicator
-from stock_indicators._cstypes import List as CsList
 from stock_indicators.indicators.common.enums import ChandelierType
 from stock_indicators.indicators.common.helpers import RemoveWarmupMixin
+from stock_indicators.indicators.common.indicator import Indicator, calculate_indicator
 from stock_indicators.indicators.common.results import IndicatorResults, ResultBase
 from stock_indicators.indicators.common.quote import Quote
 
 
+class ChandelierResult(ResultBase):
+    """
+    A wrapper class for a single unit of Chandelier Exit results.
+    """
+
+    @property
+    def chandelier_exit(self) -> Optional[float]:
+        return self._csdata.ChandelierExit
+
+    @chandelier_exit.setter
+    def chandelier_exit(self, value):
+        self._csdata.ChandelierExit = value
+
+
+_T = TypeVar("_T", bound=ChandelierResult)
+class ChandelierResults(RemoveWarmupMixin, IndicatorResults[_T]):
+    """
+    A wrapper class for the list of Chandelier Exit results.
+    It is exactly same with built-in `list` except for that it provides
+    some useful helper methods written in C# implementation.
+    """
+
+
+class Chandelier(Indicator):
+    is_chainee = False
+    is_chainor = True
+
+    indicator_method = CsIndicator.GetChandelier[Quote]
+    chaining_method = None
+
+    list_wrap_class = ChandelierResults
+    unit_wrap_class = ChandelierResult
+
+
+@calculate_indicator(indicator=Chandelier())
 def get_chandelier(quotes: Iterable[Quote], lookback_periods: int = 22,
-                   multiplier: float = 3, chandelier_type: ChandelierType = ChandelierType.LONG):
+                   multiplier: float = 3, chandelier_type: ChandelierType = ChandelierType.LONG) -> ChandelierResults[ChandelierResult]:
     """Get Chandelier Exit calculated.
 
     Chandelier Exit is typically used for stop-loss and can be
@@ -36,29 +71,4 @@ def get_chandelier(quotes: Iterable[Quote], lookback_periods: int = 22,
          - [Chandelier Exit Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/Chandelier/#content)
          - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
     """
-    results = CsIndicator.GetChandelier[Quote](CsList(Quote, quotes), lookback_periods,
-                                               multiplier, chandelier_type.cs_value)
-    return ChandelierResults(results, ChandelierResult)
-
-
-class ChandelierResult(ResultBase):
-    """
-    A wrapper class for a single unit of Chandelier Exit results.
-    """
-
-    @property
-    def chandelier_exit(self) -> Optional[float]:
-        return self._csdata.ChandelierExit
-
-    @chandelier_exit.setter
-    def chandelier_exit(self, value):
-        self._csdata.ChandelierExit = value
-
-
-_T = TypeVar("_T", bound=ChandelierResult)
-class ChandelierResults(RemoveWarmupMixin, IndicatorResults[_T]):
-    """
-    A wrapper class for the list of Chandelier Exit results.
-    It is exactly same with built-in `list` except for that it provides
-    some useful helper methods written in C# implementation.
-    """
+    return (quotes, lookback_periods, multiplier, chandelier_type.cs_value)
--- tests/test_chandelier.py
+++ tests/test_chandelier.py
@@ -1,5 +1,6 @@
 import pytest
 from stock_indicators import indicators
+from stock_indicators.indicators.common.chain import IndicatorChain
 from stock_indicators.indicators.common.enums import ChandelierType
 
 class TestChandelier:
@@ -19,6 +20,22 @@ class TestChandelier:
 
         r = short_results[501]
         assert 246.4240 == round(float(r.chandelier_exit), 4)
+
+    def test_chainor(self, quotes):
+        results = IndicatorChain.use_quotes(quotes)\
+            .add(indicators.get_chandelier, 22)\
+            .add(indicators.get_sma, 10)\
+            .calc()
+
+        assert 502 == len(results)
+        assert 471 == len(list(filter(lambda x: x.sma is not None, results)))
+
+    def test_chainee(self, quotes):
+        with pytest.raises(ValueError):
+            results = IndicatorChain.use_quotes(quotes)\
+            .add(indicators.get_sma)\
+            .add(indicators.get_chandelier)\
+            .calc()
 
     def test_bad_data(self, bad_quotes):
         r = indicators.get_chandelier(bad_quotes, 15, 2)
