ac187140b6e9f8c7183d22dc59993556a6959d67
DongGeon Lee <secmatth1996@gmail.com>
2023-03-01 14:19:35 +0900

    Use Indicator class for DEMA

--- stock_indicators/indicators/dema.py
+++ stock_indicators/indicators/dema.py
@@ -1,34 +1,10 @@
 from typing import Iterable, Optional, TypeVar
 
 from stock_indicators._cslib import CsIndicator
-from stock_indicators._cstypes import List as CsList
 from stock_indicators.indicators.common.helpers import RemoveWarmupMixin
+from stock_indicators.indicators.common.indicator import Indicator, calculate_indicator
 from stock_indicators.indicators.common.results import IndicatorResults, ResultBase
 from stock_indicators.indicators.common.quote import Quote
-
-
-def get_dema(quotes: Iterable[Quote], lookback_periods: int):
-    """Get DEMA calculated.
-
-    Double Exponential Moving Average (DEMA) of the Close price.
-
-    Parameters:
-        `quotes` : Iterable[Quote]
-            Historical price quotes.
-
-        `lookback_periods` : int
-            Number of periods in the lookback window.
-
-    Returns:
-        `DEMAResults[DEMAResult]`
-            DEMAResults is list of DEMAResult with providing useful helper methods.
-
-    See more:
-         - [DEMA Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/DoubleEma/#content)
-         - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
-    """
-    results = CsIndicator.GetDema[Quote](CsList(Quote, quotes), lookback_periods)
-    return DEMAResults(results, DEMAResult)
 
 
 class DEMAResult(ResultBase):
@@ -52,3 +28,37 @@ class DEMAResults(RemoveWarmupMixin, IndicatorResults[_T]):
     It is exactly same with built-in `list` except for that it provides
     some useful helper methods written in C# implementation.
     """
+
+class DEMA(Indicator):
+    is_chainee = True
+    is_chainor = True
+
+    indicator_method = CsIndicator.GetDema[Quote]
+    chaining_method = CsIndicator.GetDema
+
+    list_wrap_class = DEMAResults
+    unit_wrap_class = DEMAResult
+
+
+@calculate_indicator(indicator=DEMA())
+def get_dema(quotes: Iterable[Quote], lookback_periods: int) -> DEMAResults[DEMAResult]:
+    """Get DEMA calculated.
+
+    Double Exponential Moving Average (DEMA) of the Close price.
+
+    Parameters:
+        `quotes` : Iterable[Quote]
+            Historical price quotes.
+
+        `lookback_periods` : int
+            Number of periods in the lookback window.
+
+    Returns:
+        `DEMAResults[DEMAResult]`
+            DEMAResults is list of DEMAResult with providing useful helper methods.
+
+    See more:
+         - [DEMA Reference](https://daveskender.github.io/Stock.Indicators.Python/indicators/DoubleEma/#content)
+         - [Helper Methods](https://daveskender.github.io/Stock.Indicators.Python/utilities/#content)
+    """
+    return (quotes, lookback_periods)
--- tests/test_dema.py
+++ tests/test_dema.py
@@ -1,5 +1,6 @@
 import pytest
 from stock_indicators import indicators
+from stock_indicators.indicators.common.chain import IndicatorChain
 
 class TestDoubleEMA:
     def test_standard(self, quotes):
@@ -16,7 +17,25 @@ class TestDoubleEMA:
         
         r = results[501]
         assert 241.1677 == round(float(r.dema), 4)
+     
+    def test_chainor(self, quotes):
+        results = IndicatorChain.use_quotes(quotes)\
+            .add(indicators.get_dema, 20)\
+            .add(indicators.get_sma, 10)\
+            .calc()
+
+        assert 502 == len(results)
+        assert 474 == len(list(filter(lambda x: x.sma is not None, results)))
+
+    def test_chainee(self, quotes):
+        results = IndicatorChain.use_quotes(quotes)\
+            .add(indicators.get_sma, 2)\
+            .add(indicators.get_dema, 20)\
+            .calc()
         
+        assert 502 == len(results)
+        assert 482 == len(list(filter(lambda x: x.dema is not None, results)))
+
     def test_bad_data(self, bad_quotes):
         r = indicators.get_dema(bad_quotes, 15)
         assert 502 == len(r)
