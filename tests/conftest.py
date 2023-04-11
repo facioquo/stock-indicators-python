9041ec57f4b493bb8217961345b44220d92575ca
axaxax <67626131+alexpvpmindustry@users.noreply.github.com>
2023-04-12 00:56:32 +0800

    fix parse dates

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,7 +31,9 @@ def parse_decimal(value):
 def parse_date(date_str):
     """Parse date value. Input format must be '%Y-%m-%d' """
     try:
-        return datetime.strptime(date_str, '%Y-%m-%d')
+        if len(date_str)==10:
+            return datetime.strptime(date_str, '%Y-%m-%d ')
+        return datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
     except ValueError:
         return datetime.now()
 
