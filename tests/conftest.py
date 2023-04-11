22aa952d612cdf2d2475aaa71b2b9f4be5616c78
axaxax <67626131+alexpvpmindustry@users.noreply.github.com>
2023-04-12 01:03:52 +0800

    more robust

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,7 +31,7 @@ def parse_decimal(value):
 def parse_date(date_str):
     """Parse date value. Input format must be '%Y-%m-%d' """
     try:
-        if len(date_str)==10:
+        if len(date_str)<=10:
             return datetime.strptime(date_str, '%Y-%m-%d ')
         return datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
     except ValueError:
