c6526252ca5d1835325c00625630edad15c710a5
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2025-01-02 19:06:52 -0500

    setup local GH Action runner (partially)

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,6 +8,3 @@ pip install -r requirements.txt
 
 # install test dependencies
 pip install -r requirements-test.txt
-
-# install GitHub Actions emulator (act)
-curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
